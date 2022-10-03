bb007dab99c35d3040855bf6370fcd28d24bdab7
RaulPiedra <terfezio@gmail.com>
2022-10-03 23:42:23 +0200

    3/10/2022 23:42

---
+++ src/main/java/com/terfezio/Ejercicio1_6_2.java
@@ -0,0 +1,22 @@
+package com.terfezio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Ejercicio1_6_2 {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("Comando invalido. Uso: ");
+            System.out.println("comando origen texto destino");
+        }
+        else {
+            File outputFile = new File(args[2]);
+            ProcessBuilder processBuilder = new ProcessBuilder("grep", args[2], args[1]);
+            processBuilder.inheritIO();
+            processBuilder.directory(new File("/home/raul/"));
+            processBuilder.redirectOutput(outputFile);
+            processBuilder.start();
+
+        }
+    }
+}
