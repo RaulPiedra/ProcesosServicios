237e2ce096ba6c0ec52287a5b2f14bae55383f9c
RaulPiedra <terfezio@gmail.com>
2022-12-17 01:13:42 +0100

    arreglado cliente

--- src/main/java/com/terfezio/tema3/fileserver/FileClient.java
+++ src/main/java/com/terfezio/tema3/fileserver/FileClient.java
@@ -9,10 +9,9 @@ public class FileClient {
         // Definimos los parámetros de conexión
         InetAddress direccion; // La IP o Dirección de conexión
         Socket servidor; // Socket para conectarnos a un servidor u otra máquina
-        int numCliente = 0; // Mi número de cliente
         int PUERTO = 5000;  // Puerto de conexión
 
-        System.out.println("Soy el cliente e intento conectarme");
+        System.out.println("Cliente conectando...");
 
         try {
             // Vamos a indicar la dirección de conexión
@@ -21,34 +20,32 @@ public class FileClient {
             servidor = new Socket(direccion, PUERTO);
             // Operamos con la conexión. En este caso recibimos los datos que nos mandan
             System.out.println("Conexión realizada con éxito");
-            //BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-            //System.out.print("Introduzca ruta y nombre de un archivo: ");
-            //String filePath = kb.readLine();
+
+            BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Introduzca ruta y nombre de un archivo: ");
+            String filePath = kb.readLine();
 
             OutputStream outputStream = servidor.getOutputStream();
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-            //DataOutputStream writer = new DataOutputStream(outputStream);
-            BufferedWriter writer = new BufferedWriter(outputStreamWriter);
-            //writer.writeUTF(filePath);
-            writer.write("recontrahola\n");
-            writer.flush();
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-            InputStream inputStream = servidor.getInputStream();
-            //BufferedReader fileBufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            //String line = fileBufferedReader.readLine();
-            //while (line != null) {
-            //    System.out.println(line);
-            //    line = fileBufferedReader.readLine();
-            //}
-            DataInputStream dataInputStream = new DataInputStream(inputStream);
-            String line = dataInputStream.readUTF();
-            while (line != null) {
+            bufferedWriter.write(filePath + "\n");            
+            bufferedWriter.flush();
+
+            InputStream inputStream = servidor.getInputStream();            
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            
+            String line;
+            System.out.println("===Inicio del archivo=============");
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
-                line = dataInputStream.readUTF();
+                
             }
+            System.out.println("===Fin del archivo================");
 
             servidor.close();
-            System.out.println("Soy el cliente y cierro la conexión");
+            System.out.println("Cliente cerrando la conexión");
         } catch (Exception e) {
             e.printStackTrace();
         }
